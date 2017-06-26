2286730b3cae68925a1dc2b1ab10ad1b4aee0d6a
k232629 <olhakaraiman@gmail.com>
2017-06-26 16:06:48 +0300

    add music to menu and select menu

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour {
+	public MyButton musicButton;
+	public Sprite soundOn;
+	public Sprite soundOff;
+	public AudioClip musicClip = null;
+	AudioSource musicSource = null;
+	bool music;
 	public UILabel bioFirst;
 	public UILabel bioSecond;
 	public MyButton nextButton1;
@@ -24,6 +30,15 @@ public class Controller : MonoBehaviour {
 	public int numberOfPlayers;
 	// Use this for initialization
 	void Start () {
+		musicSource = gameObject.AddComponent<AudioSource>();
+		musicSource.clip = musicClip;
+		musicSource.loop = true;
+		if(SoundManager.Instance.isSoundOn())
+			musicSource.Play ();
+		music = SoundManager.Instance.isSoundOn ();
+		changeMusic ();
+		changeMusic ();
+		musicButton.signalOnClick.AddListener (changeMusic);
 		numberOfPlayers--;
 		countFirst = 0;
 		countSecond = 1;
@@ -100,6 +115,18 @@ public class Controller : MonoBehaviour {
 		SceneManager.LoadScene ("Menu");
 	}
 
-
+	void changeMusic(){
+		if (music) {
+			musicSource.Pause ();
+			musicButton.GetComponent<UIButton> ().normalSprite2D = soundOff;
+			SoundManager.Instance.setSoundOn (false);
+			music = false;
+		} else {
+			musicSource.Play ();
+			musicButton.GetComponent<UIButton> ().normalSprite2D = soundOn;
+			SoundManager.Instance.setSoundOn (true);
+			music = true;
+		}
+	}
 
 }
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour {
+	public MyButton musicButton;
 	public MyButton trainingButton;
 	public MyButton twoPlayersByGoolsButton;
 	public MyButton twoPlayersByTimeButton;
+	public Sprite soundOn;
+	public Sprite soundOff;
+	public AudioClip musicClip = null;
+	AudioSource musicSource = null;
+	bool music;
 	// Use this for initialization
 	void Start () {
+		musicSource = gameObject.AddComponent<AudioSource>();
+		musicSource.clip = musicClip;
+		musicSource.loop = true;
+		if(SoundManager.Instance.isSoundOn())
+			musicSource.Play ();
+		music = SoundManager.Instance.isSoundOn ();
+		changeMusic ();
+		changeMusic ();
 		trainingButton.signalOnClick.AddListener (training);
 		twoPlayersByGoolsButton.signalOnClick.AddListener (twoPlayersByGools);
 		twoPlayersByTimeButton.signalOnClick.AddListener (twoPlayersByTime);
+		musicButton.signalOnClick.AddListener (changeMusic);
 	}
 
 	void training(){
-		SceneManager.LoadScene ("Select player");
+		SceneManager.LoadScene ("Select two players");
 		PlayerPrefs.SetString ("mode", Mode.training);
 	}
 
@@ -26,5 +41,19 @@ public class MenuScript : MonoBehaviour {
 	void twoPlayersByTime(){
 		SceneManager.LoadScene ("Select two players");
 		PlayerPrefs.SetString ("mode", Mode.twoPlayersByTime);
+	}
+
+	void changeMusic(){
+		if (music) {
+			musicSource.Pause ();
+			musicButton.GetComponent<UIButton> ().normalSprite2D = soundOff;
+			SoundManager.Instance.setSoundOn (false);
+			music = false;
+		} else {
+			musicSource.Play ();
+			musicButton.GetComponent<UIButton> ().normalSprite2D = soundOn;
+			SoundManager.Instance.setSoundOn (true);
+			music = true;
+		}
 	}
 }
